4475bca2a703a4929b134ad6ea997576f0a0d13b
ss695847 <756682837@qq.com>
2025-06-02 13:50:57 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://aitanbo.cn:21114".to_owned()
+    "https://aitanbo.cn".to_owned()
 }
 
 #[inline]
